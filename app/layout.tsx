367dcf68fbeb28ece58cfe9ec176ec846ef714f3
Jai Bhalla <jaibhalla025@gmail.com>
2025-09-07 19:55:54 +0530

    some more change

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -63,6 +64,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Analytics/>
       </body>
     </html>
   );
